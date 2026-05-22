9e8715a33a3a39143d543273947d589e4d3d882f
ggb2580 <3581695569@qq.com>
2026-05-22 11:52:59 +0800

    loadbalancer-负载均衡

---
+++ springcloud-alibaba-demo/order-loadbalancer/src/main/java/com/alibaba/demo/OrderLoadbalancerApplication.java
@@ -0,0 +1,23 @@
+package com.alibaba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Say my name
+ */
+@SpringBootApplication
+public class OrderLoadbalancerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderLoadbalancerApplication.class,args);
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+}
---
+++ springcloud-alibaba-demo/order-loadbalancer/src/main/java/com/alibaba/demo/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.alibaba.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Say my name
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/add")
+    public String add(){
+        System.out.println("下单成功");
+        String message = restTemplate.getForObject("http://stock-service/stock/reduce", String.class);
+        return "下单成功"+message;
+    }
+}
